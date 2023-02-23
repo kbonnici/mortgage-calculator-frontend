f620ee319ccd4586b8012554efc2cb152d0e9823
Karl Bonnici <karl.bonnici100@gmail.com>
2023-02-23 13:01:22 -0800

    initial commit

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,8 @@
+export default function Header(){
+    return (
+        <>
+            <h1 className="header-1 pb-4">BC Mortgage Calculator</h1>
+            <span className="text-xs">Developed by Karl Bonnici (@kbonnici)</span>
+        </>
+    )
+}
