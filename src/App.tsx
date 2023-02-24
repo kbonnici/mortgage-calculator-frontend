1844b9733066539c0f568ec6625599062960cab7
Karl Bonnici <karl.bonnici100@gmail.com>
2023-02-23 17:03:37 -0800

    add result display to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
-import './App.css'
-import Form from './components/form/Form'
-import Heading from './components/header/Header'
+import "./App.css";
+import { useState } from "react";
+import Form from "./components/form/Form";
+import Heading from "./components/header/Header";
+import ResultDisplay from "./components/resultDisplay/ResultDisplay";
 
 function App() {
+  const [monthlyPayment, setMonthlyPayment] = useState<number | string>("");
+
   return (
     <>
-      <Heading/>
-      <Form/>
+      <Heading />
+      <Form
+        monthlyPayment={monthlyPayment}
+        setMonthlyPayment={setMonthlyPayment}
+      />
+      <ResultDisplay monthlyPayment={monthlyPayment} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
